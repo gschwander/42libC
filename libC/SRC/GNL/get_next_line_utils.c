00e7780a3f9e50c57f15be7352fa21677c757440
Gautier Schwander <gschwand@f2r2s12.clusters.42paris.fr>
2025-06-16 16:04:54 +0200

    norm GNL

--- libC/SRC/GNL/get_next_line_utils.c
+++ libC/SRC/GNL/get_next_line_utils.c
@@ -1,63 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 16:04:04 by gschwand          #+#    #+#             */
+/*   Updated: 2025/06/16 16:04:05 by gschwand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-
-int find_chr_gnl(char *str, char c)
+int	find_chr_gnl(char *str, char c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] == c)
-            return (i);
-        i++;
-    }
-    return (-1);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
-void ft_bezero_gnl(char *str, int n)
+void	ft_bezero_gnl(char *str, int n)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < n)
-    {
-        str[i] = '\0';
-        i++;
-    }
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
 }
 
-int ft_strlen_gnl(char *str)
+int	ft_strlen_gnl(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
-void ft_strcpy_gnl(char *dest, char *src)
+void	ft_strcpy_gnl(char *dest, char *src)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 }
 
-char *ft_strdup_gnl(char *str)
+char	*ft_strdup_gnl(char *str)
 {
-    char *new_str;
+	char	*new_str;
 
-    new_str = (char *)malloc(sizeof(char) * (ft_strlen_gnl(str) + 1));
-    if (new_str == NULL)
-        return (NULL);
-    ft_strcpy_gnl(new_str, str);
-    return (new_str);
+	new_str = (char *)malloc(sizeof(char) * (ft_strlen_gnl(str) + 1));
+	if (new_str == NULL)
+		return (NULL);
+	ft_strcpy_gnl(new_str, str);
+	return (new_str);
 }
-
--- libC/include/get_next_line.h
+++ libC/include/get_next_line.h
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 16:04:18 by gschwand          #+#    #+#             */
+/*   Updated: 2025/06/16 16:04:19 by gschwand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # define BUFFER_SIZE 32
 
-int find_chr_gnl(char *str, char c);
-void ft_bezero_gnl(char *str, int n);
-int ft_strlen_gnl(char *str);
-void ft_strcpy_gnl(char *dest, char *src);
-char *ft_strdup_gnl(char *str);
-char *get_next_line(int fd);
+int		find_chr_gnl(char *str, char c);
+void	ft_bezero_gnl(char *str, int n);
+int		ft_strlen_gnl(char *str);
+void	ft_strcpy_gnl(char *dest, char *src);
+char	*ft_strdup_gnl(char *str);
+char	*get_next_line(int fd);
 
 #endif
